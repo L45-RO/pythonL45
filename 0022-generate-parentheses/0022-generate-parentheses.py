f9f0e5fa1e31398baa638c6525ff9eeb2d833429
LUCKY <likithreddy478@gmail.com>
2025-10-25 16:26:44 +0530

    Time: 0 ms (100%), Space: 17.9 MB (69.83%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        results = []
+
+        def backtrack(path, a, b):
+            if len(path) == n * 2:
+                results.append(path)
+                return
+            if a < n:
+                backtrack(path + "(", a + 1, b)
+            if b < a:
+                backtrack(path + ")", a, b + 1)
+
+        backtrack("(", 1, 0)
+        return results
